01b4ca9b7cd9d6045c12dcf25b451916b144ffcf
terencegriffithCB5 <terence.griffith92@gmail.com>
2019-08-21 20:10:47 -0400

    added the favorite controller page

---
+++ controllers/favoriteAnime.js
@@ -0,0 +1,50 @@
+/* Step 1 import express
+ *
+ */
+const express = require('express')
+
+/* Step 2
+ *
+ * Import the api files from the models
+ *
+ * TODO: change the file path to the models file you'll need to use.
+ * TODO: rename this from `templateApi` to something more sensible (e.g:
+ * `shopsAPI`)
+ *
+ * NOTE: You may need to import more than one API to create the 
+ * controller you need.
+ * 
+ */
+const animeApi = require('../models/favoriteAnime.js')
+
+/* Step 3 
+ * 
+ * Create a new router.
+ *
+ * the router will "contain" all the request handlers that you define in this file.
+ * TODO: rename this from templateRouter to something that makes sense. (e.g:
+ * `shopRouter`)
+ */
+const animeRouter = express.Router()
+
+/* Step 4
+ * 
+ * TODO: Put all request handlers here
+ */
+
+/* Step 5
+ *
+ * TODO: delete this handler; it's just a sample
+ */ 
+// animeRouter.get('/', (req, res) => {
+//   res.send(animeApi.getHelloWorldString())
+// })
+
+/* Step 6
+ *
+ * Export the router from the file.
+ *
+ */
+module.exports = {
+  animeRouter
+}
