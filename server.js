1820d34fbf0a7e290cc0ebf41acea1642387225a
terencegriffithCB5 <terence.griffith92@gmail.com>
2019-08-21 20:07:30 -0400

    made sure the server ran properly

--- server.js
+++ server.js
@@ -7,9 +7,7 @@ const methodOverride = require('method-override')
  * import routers from controllers/
  *
  */
-const { animeRouter } = require('./controllers/favoriteAnime.js')
-const { interestRouter } = require('./controllers/interest.js')
-const { reviewRouter } = require('./controllers/reviews.js')
+const { templateRouter } = require('./controllers/template.js')
 
 
 /* Step 3
@@ -53,9 +51,8 @@ app.set('view engine', 'hbs')
  * add router for the application to use. The first argument is a prefix to all
  * the paths defined in the router.
  */
-app.use('/anime', animeRouter)
-app.use('/anime', interestRouter)
-app.use('/anime', reviewRouter)
+app.use('/anime', templateRouter)
+
 
 /* Step 5
  *
